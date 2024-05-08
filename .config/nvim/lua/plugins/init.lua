b8ca73114ff2b6668ed7d04b3dde1ef559b389f4
mike stone <michaeljacobstone@gmail.com>
2024-05-08 11:57:19 +0200

    latency speedup

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -11,11 +11,6 @@ return {
 	},
 
 	{
-		"echasnovski/mini.statusline",
-		version = "*",
-		opts = {},
-	},
-	{
 		"nvimdev/dashboard-nvim",
 		event = "VimEnter",
 		config = function()
@@ -35,9 +30,36 @@ return {
 	{ "ThePrimeagen/vim-be-good" },
 	{ "numToStr/Comment.nvim", opts = {} },
 	{
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
+		"stevearc/profile.nvim",
+		config = function()
+			local should_profile = os.getenv("NVIM_PROFILE")
+			if should_profile then
+				require("profile").instrument_autocmds()
+				if should_profile:lower():match("^start") then
+					require("profile").start("*")
+				else
+					require("profile").instrument("*")
+				end
+			end
+
+			local function toggle_profile()
+				local prof = require("profile")
+				if prof.is_recording() then
+					prof.stop()
+					vim.ui.input(
+						{ prompt = "Save profile to:", completion = "file", default = "profile.json" },
+						function(filename)
+							if filename then
+								prof.export(filename)
+								vim.notify(string.format("Wrote %s", filename))
+							end
+						end
+					)
+				else
+					prof.start("*")
+				end
+			end
+			vim.keymap.set("", "<f1>", toggle_profile)
+		end,
 	},
 }
