d506949f8b331da3fd8b0fc7b668786d547e47e8
mike stone <michaeljacobstone@gmail.com>
2024-06-19 12:55:30 +0200

    zig fix

---
+++ .config/nvim/after/ftplugin/zig.lua
@@ -0,0 +1,22 @@
+local ns = vim.api.nvim_create_namespace("zig-on-save")
+local group = vim.api.nvim_create_augroup("zig-on-save", { clear = true })
+local buffer = vim.api.nvim_get_current_buf()
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = group,
+  buffer = buffer,
+  command = "silent! make! | redraw!",
+})
+
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+  group = group,
+  buffer = buffer,
+  callback = function(event)
+    local qflist = vim.fn.getqflist()
+    local diagnostics = vim.diagnostic.fromqflist(qflist)
+
+    vim.diagnostic.reset(ns, event.buf) -- don't actually know if this is necessary, fafo
+    vim.diagnostic.set(ns, event.buf, diagnostics)
+  end,
+})
+vim.cmd("silent! make! | redraw!")
