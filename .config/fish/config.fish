a33901ab8d4906bf981f00a921bfdb2a588e5691
mike stone <michaeljacobstone@gmail.com>
2025-11-02 13:57:01 +0100

    add niri

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,35 +1,20 @@
 #!/usr/bin/env fish
 # Export bun paths
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
 set -U fish_user_paths $HOME/go/bin $fish_user_paths
 set -U fish_greeting
 starship init fish | source
 zoxide init fish | source
 alias dotfiles 'git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
-
-# Retrieve the current terminal identifier
-set current_tty (tty)
-
-# Check if the current session is a login shell
-if status --is-login
-    # Ensure we're on tty1
-    if test "$current_tty" = "/dev/tty1"
-        # Run the Hyprland script directly; check its success or failure
-        if ~/hyprland.fish
-            echo "goodbye, now logging out"
-            exit 0
-        else
-            echo "hyprland.fish failed"
-            echo "refusing autologin without hyprland on tty1"
-            exit 1
-        end
-    end
-end
 bind \e\[C 'commandline -f forward-char'
 bind \e 'commandline -f forward-char'
-set -x BROWSER zen-browser
+set -x BROWSER helium-browser
+set -x SYSTEMD_EDITOR nvim
 source ~/.config/fish/keys.fish
 
-
 fish_add_path $HOME/.local/bin
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+#exec ~./hyprland.fish
